52d6845609810b88f943e7f138a5702350668a23
fwew2 <fodae8411@gmail.com>
2025-03-23 20:57:15 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -31,40 +31,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-const swiper = new Swiper(".mySwiper", {
-    loop: true,            // تشغيل السلايدر بشكل دائري
-    navigation: {          // أزرار التنقل
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {          // النقاط السفلية
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {            // تشغيل تلقائي
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-});
-
-function showTab(tabId) {
-    // إخفاء جميع المحتويات
-    document.querySelectorAll('.tab-content').forEach(content => {
-        content.classList.remove('active');
-    });
-
-    // إظهار المحتوى المطلوب
-    document.getElementById(tabId).classList.add('active');
-
-    // تحديث الـ Tabs الفعالة
-    document.querySelectorAll('.tab').forEach(tab => {
-        tab.classList.remove('active');
-    });
-
-    // تحديد التاب الفعال
-    event.target.classList.add('active');
-}
-
-
 
 
