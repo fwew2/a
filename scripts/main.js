dfad369270fb9edaf8b778f9295ca1db236c9d7b
fwew2 <fodae8411@gmail.com>
2025-04-05 17:12:41 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+window.addEventListener("scroll",function(){
+    let scrollY = window.scrollY;
+    if(scrollY>=200){
+        scrollbtn.style.display="block";
+    }
+    else{
+        scrollbtn.style.display="none";
+    }
+
+});
+
+
+
+
+let scrollbtn=document.getElementById("scrollbtn");
+scrollbtn.addEventListener("click",function(){     
+    window.scrollTo(0,0);
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuBtn.addEventListener("click", function () {
+        mobileMenu.classList.toggle("active");
+        menuBtn.classList.toggle("active");
+    });
+});
+
+
+
